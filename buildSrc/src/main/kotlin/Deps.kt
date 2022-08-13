e33f1109bf3b1b428ae71b7e24c93e54bf54b010
binhyul <gkswlssla@gmail.com>
2022-08-14 05:20:55 +0900

    build: 사용안하는 deps, versions 제거

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -2,41 +2,20 @@ object Deps {
     object Android {
         object X {
             const val core = "androidx.core:core-ktx:${Versions.coreVersion}"
-            const val lifecycleProcess =
-                "androidx.lifecycle:lifecycle-process:${Versions.lifecycleVersion}"
-            const val lifecycleViewModel =
-                "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycleVersion}"
-            const val lifecycleLiveData =
-                "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycleVersion}"
             const val lifecycleRuntime =
                 "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleVersion}"
-            const val activity = "androidx.activity:activity-ktx:${Versions.activityVersion}"
-            const val fragment = "androidx.fragment:fragment-ktx:${Versions.fragmentVersion}"
-            const val multidex = "androidx.multidex:multidex:${Versions.mutidexVersion}"
             const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompatVersion}"
 
 
-            const val recyclerView =
-                "androidx.recyclerview:recyclerview:${Versions.recyclerViewVersion}"
             const val constraintLayout =
                 "androidx.constraintlayout:constraintlayout:${Versions.constraintLayoutVersion}"
-            const val coordinatorLayout =
-                "androidx.coordinatorlayout:coordinatorlayout:${Versions.coordinatorLayoutVersion}"
         }
 
         const val material = "com.google.android.material:material:${Versions.materialVersion}"
     }
 
-    object Coroutine {
-        const val core =
-            "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutineVersion}"
-        const val android =
-            "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutineVersion}"
-    }
-
     object Paging {
         const val runtime = "androidx.paging:paging-runtime:${Versions.pagingVersion}"
-        const val rx = "androidx.paging:paging-rxjava2:${Versions.pagingVersion}"
     }
 
     object Network {
@@ -45,7 +24,6 @@ object Deps {
             "com.squareup.retrofit2:converter-gson:${Versions.retrofitVersion}"
         const val retrofitRx = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofitVersion}"
 
-        const val okHttp = "com.squareup.okhttp3:okhttp:${Versions.okHttpVersion}"
         const val okHttpLogging =
             "com.squareup.okhttp3:logging-interceptor:${Versions.okHttpVersion}"
         const val okHttpUrlConnection =
@@ -54,13 +32,8 @@ object Deps {
         const val apacheCommon = "org.apache.commons:commons-lang3:${Versions.apacheCommonVersion}"
     }
 
-    const val leakCanary =
-        "com.squareup.leakcanary:leakcanary-android:${Versions.leakCanaryVersion}"
-    const val gson = "com.google.code.gson:gson:${Versions.gsonVersion}"
     const val glide = "com.github.bumptech.glide:glide:${Versions.glideVersion}"
     const val glideCompiler = "com.github.bumptech.glide:compiler:${Versions.glideVersion}"
-    const val installReferrer =
-        "com.android.installreferrer:installreferrer:${Versions.installReferrerVersion}"
 
     object AndroidTest {
         const val junit = "junit:junit:${Versions.AndroidTest.junitVersion}"
@@ -79,12 +52,6 @@ object Deps {
         const val hiltAndroid = "com.google.dagger:hilt-android:${Versions.Hilt.hiltAndroidVersion}"
         const val hiltAndroidCompiler =
             "com.google.dagger:hilt-android-compiler:${Versions.Hilt.hiltAndroidVersion}"
-        const val hiltWork =
-            "androidx.hilt:hilt-work:${Versions.Hilt.hiltAndroidxVersion}"
-        const val hiltCompiler =
-            "androidx.hilt:hilt-compiler:${Versions.Hilt.hiltAndroidxVersion}"
-        const val hiltTest =
-            "com.google.dagger:hilt-android-testing:${Versions.Hilt.hiltAndroidVersion}"
     }
 
     object Navigation {
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -17,36 +17,25 @@ object Versions {
 
     object Hilt {
         const val hiltAndroidVersion = "2.42"
-        const val hiltAndroidxVersion = "1.0.0"
     }
 
     const val kotlinVersion = "1.7.0"
-    const val coroutineVersion = "1.6.0"
 
     const val pagingVersion = "3.1.1"
 
     const val materialVersion = "1.4.0"
 
-    const val activityVersion = "1.2.0"
-    const val fragmentVersion = "1.3.0"
     const val lifecycleVersion = "2.5.0"
     const val navigationVersion = "2.3.5"
 
-    const val mutidexVersion = "2.0.1"
     const val appCompatVersion = "1.3.1"
 
-    const val recyclerViewVersion = "1.2.1"
     const val constraintLayoutVersion = "2.1.4"
     const val coreVersion = "1.1.0"
-    const val coordinatorLayoutVersion = "1.1.0"
 
-    const val leakCanaryVersion = "2.8.1"
-    const val gsonVersion = "2.8.2@jar"
     const val glideVersion = "4.12.0"
 
     const val apacheCommonVersion = "3.4"
-
-    const val installReferrerVersion = "1.1"
 
     const val retrofitVersion = "2.6.4"
     const val okHttpVersion = "3.12.13"
